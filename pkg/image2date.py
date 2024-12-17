8500a902f7efcd590797d5080b6df9d25e929f15
𝘚𝘖𝘕𝘈𝘓 <josephtoppo.sxd@gmail.com>
2024-12-17 19:55:33 +0530

    Update image2date.py

--- pkg/image2date.py
+++ pkg/image2date.py
@@ -2,15 +2,12 @@ import os
 from datetime import datetime
 
 def rename_images(directory):
-    # Iterate over files in the directory
     for filename in os.listdir(directory):
         file_path = os.path.join(directory, filename)
 
-        # Process only files that are images (common formats)
         if os.path.isfile(file_path) and filename.lower().endswith((".jpg", ".jpeg", ".png", ".gif", ".bmp")):
-            # Get the modification time of the file
-            modification_time = os.path.getmtime(file_path)
-            timestamp = datetime.fromtimestamp(modification_time).strftime("%Y%m%d_%H%M%S%f")
+            modification_time = os.path.getmtime(file_path) #%S%f
+            timestamp = datetime.fromtimestamp(modification_time).strftime("%Y%m%d_%H%M%S")
 
             # Create the new filename
             file_extension = os.path.splitext(filename)[1]
@@ -28,5 +25,5 @@ def rename_images(directory):
             os.rename(file_path, renamed_file_path)
 
 # Specify the directory containing the images
-# directory_path = r"E:\T2024\12-2015"  # Replace with your directory path
-# rename_images(directory_path)
+directory_path = r"./"  # Replace with your directory path
+rename_images(directory_path)
